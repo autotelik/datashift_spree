364585cef460a6f533f828bdd5cf04bab502d52e
tom statter <studio299@autotelik.co.uk>
2014-07-08 09:37:45 +0100

    remove sandbox from git

--- spec/rspec_spree_sandbox/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-RspecSpreeSandbox::Application.config.session_store :cookie_store, key: '_rspec_spree_sandbox_session'
--- spec/rspec_spree_sandbox/spree_sandbox.thor
+++
@@ -1,5 +0,0 @@
-require 'datashift'
-require 'datashift_spree'
-
-DataShift::SpreeHelper::load_commands
-DataShift::load_commands
